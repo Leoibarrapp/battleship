7c178f22139c3d97445007990f5bb55a31957ae9
Leoibarrapp <laibarra.23@est.ucab.edu.ve>
2025-01-06 20:45:33 -0400

    higlight

--- Taller 2/js/estrategia.js
+++ Taller 2/js/estrategia.js
@@ -23,16 +23,27 @@ function crearTabla(i) {
             const celdaDiv = document.createElement('div');
             celdaDiv.className = 'position';
             celdaDiv.id = `p${i}-${letra.toLowerCase()}${j}`;
-            celdaDiv.addEventListener('dragover', permitirSoltar);
+            celdaDiv.addEventListener('dragover', highlight);
+            celdaDiv.addEventListener('dragleave', removeHighlight);
             celdaDiv.addEventListener('drop', soltar);
             board.appendChild(celdaDiv);
         }
     });
 }
 
+function highlight(event) { 
+    event.preventDefault(); 
+    event.target.classList.add('highlight'); 
+} // Función para quitar el resaltado al dejar de arrastrar  
+
+function removeHighlight(event) { 
+    event.target.classList.remove('highlight');
+}
+
 function arrastrar(event) {
     // Usar event.target.id para obtener el ID de la imagen arrastrada
     event.dataTransfer.setData("barcoId", event.target.id);
+    obtenerIdCasillaAdyacente(event.target.id).classList.add('highlight');
     console.log(`Iniciando arrastre del barco con ID: ${event.target.id}`);
 }
 
@@ -69,16 +80,16 @@ function soltar(event) {
 function colocarBarco(casilla, barcoId) {
      var imagenBarco; switch(barcoId) { 
         case 'img-portaaviones': imagenBarco = 'url("../assets/portaaviones.png")'; 
-        break; 
+            break; 
         case 'img-acorazado': imagenBarco = 'url("../assets/acorazado.png")'; 
-        break; 
+            break; 
         case 'img-crucero': imagenBarco = 'url("../assets/crucero.png")'; 
-        break; 
+            break; 
         case 'img-submarino': imagenBarco = 'url("../assets/submarino.png")'; 
-        break; 
+            break; 
         case 'img-destructor': imagenBarco = 'url("../assets/destructor.png")'; 
         break; 
-        default: imagenBarco = ''; 
+            default: imagenBarco = ''; 
     } 
     let casillaActual = document.getElementById(casilla); 
     if (casillaActual) { 
@@ -87,14 +98,17 @@ function colocarBarco(casilla, barcoId) {
     } 
 }
 
-function obtenerIdCasillaAdyacente(casilla, offset) {
-    let letra = casilla.charAt(1);
-    let numero = parseInt(casilla.slice(2));
-    let nuevoNumero = numero + offset;
+// function obtenerIdCasillaAdyacente(casilla, offset) {
+//     let letra = casilla.charAt(1);
+//     let numero = parseInt(casilla.slice(2));
+//     let nuevoNumero = numero + offset;
 
-    if (nuevoNumero > 10) return null;  // Verificar que no se salga del tablero
+//     if (nuevoNumero > 10) return null;  // Verificar que no se salga del tablero
 
-    return `p${casilla.charAt(0)}-${letra}${nuevoNumero}`;
+//     return `p${casilla.charAt(0)}-${letra}${nuevoNumero}`;
+// }
+
+function obtenerCasillasAdyacentes(casilla) {
 }
 
 // Llamar a la función para crear la tabla
